dfad8b7ba5afee17b9a5b8ff08119044b9470bc3
Didi Walakutty <didiswalakutty@gmail.com>
2024-04-22 15:12:49 +0200

    update

--- src/table_setting.c
+++ src/table_setting.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   table_setting.c                                    :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
+/*   By: marvin <marvin@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/20 20:38:28 by diwalaku      #+#    #+#                 */
-/*   Updated: 2024/04/20 21:54:18 by diwalaku      ########   odam.nl         */
+/*   Updated: 2024/04/22 15:12:38 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	check_numbers(t_table *table, char **argv)
 	int	i;
 
 	i = 0;
-	// In which struct do we set the number of philos, time to eat, die and sleep??? 
+	// In which struct do we set the number of philos, time to eat, die and sleep???
 }
 
 t_table	*set_table(char **argv)
